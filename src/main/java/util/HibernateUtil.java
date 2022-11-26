2b987221aedc8fc76fb375a639fc240a66991079
Hoangnt95 <badboyy.nb@gmail.com>
2022-11-26 22:15:24 +0700

    test email

--- src/main/java/util/HibernateUtil.java
+++ src/main/java/util/HibernateUtil.java
@@ -49,7 +49,7 @@ public class HibernateUtil {
         properties.put(Environment.SHOW_SQL, "true");
 
         //gen DB tự động
-        properties.put(Environment.HBM2DDL_AUTO, "create");
+//        properties.put(Environment.HBM2DDL_AUTO, "create");
         return properties;
     }
 
